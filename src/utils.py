1382338564143b403334829a5057fc780a79d7f3
pramoprasanna <pramopras@gmail.com>
2024-05-20 22:58:11 +0530

    Add Model Building Module

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
 from src.logger import logging
@@ -13,5 +14,20 @@ def save_object(file_path, obj):
         with open(file_path,"wb") as file_obj:
             dill.dump(obj, file_obj)
         logging.info("File Saved Successfully")
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train,Y_train,X_Test,Y_test,models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train,Y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_Test)
+            train_model_score = r2_score(Y_train,y_train_pred)
+            test_model_score = r2_score(Y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
     except Exception as e:
         raise CustomException(e, sys)
